1f215b2fa34d07bad9b0c2a7e276310b37f5972d
Badar Abbas <142784854+RaiBadarAbbas92@users.noreply.github.com>
2024-06-01 07:26:51 +0500

    TypeScript from begginer

---
+++ index.ts
@@ -0,0 +1,98 @@
+// Typescript is superset of jvascript
+console.log("Hello World");
+
+// Getting start
+// Fundamentals
+// Variables
+var num =5
+var num =7
+
+let digi=98
+let figi=39
+
+const pi=3.14
+const i=3.14
+
+console.log(num)
+console.log(digi);
+console.log(pi);
+
+
+// Data Types
+// String , Number , Boolean , Undefinde , Null
+var string=""
+var number=192
+var bollean=true 
+var nulll="Empty"
+
+console.log(string);
+
+console.log(number);
+
+console.log(bollean);
+
+console.log(nulll);
+
+let myname="Badar Abbas"
+let message="Hello"+myname
+console.log(message);
+
+// Find data type of variable
+let numer=92
+let mname="Badar"
+let istrue=false
+let Empty;
+
+console.log(typeof Empty); //Undefinded
+console.log(typeof istrue); // Boolean
+console.log(typeof mname)   //String
+
+// Operators
+// Arithmetic operators:
+let num1=7
+let num2=4
+
+console.log(num1+num2);
+
+console.log(num1-num2);
+
+console.log(num1*num2);
+
+console.log(num1/num2);
+
+console.log(num1%num2);
+
+console.log(num1**num2);
+
+// Assignment operators:
+
+let num3=7
+let num4=4
+
+num3+=num4
+console.log(num3);
+
+// Compression operators
+let num5= 10
+console.log(num5==10);
+console.log(num5===40);
+console.log(num5 !=9);
+console.log(num5>6);
+console.log(num5<10);
+console.log(num5>=10);
+console.log(num5<=5);
+
+//Logical Operator
+let  n=19
+console.log(n >= 5 && n < 10); //
+console.log(n > 5 && n < 10); //
+console.log(n >= 5 || n < 10); //
+console.log(n > 5 || n < 10); //
+console.log(!(n < 10)); //
+console.log(!(n > 10)); // 
+
+
+
+
+
+
