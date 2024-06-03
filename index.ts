c45fcd8ef7498733479bb48fcf71d35ca8b428ff
Badar Abbas <142784854+RaiBadarAbbas92@users.noreply.github.com>
2024-06-03 08:49:01 +0500

    update

--- index.ts
+++ index.ts
@@ -93,6 +93,94 @@ console.log(!(n > 10)); //
 
 
 
+//  if else 
+let country="Pakistan"
+if(country=="Pakistan"){
+    console.log("Correct")
+}
+else{
+    console.log("Wrong")
+}
 
+// if else if
+let country1="Pakistan"
+if(country1=="Pakistan"){
+    console.log("Correct")
+    }
+    else if(country1=="India"){
+        console.log("Correct") 
+        }
+        else{
+            console.log("Wrong")
+            }           // 
 
+//  Function
+function add(){
+    let a=10
+    let b=20
+    let c=a+b
+    console.log(c)
+}
+add()
 
+//  Function with parameter
+function addi(a: number,b: number){
+    let c=a+b
+    console.log(c)
+    }
+addi(10,20)
+
+function favrotenumber(){
+    return 100;
+}
+favrotenumber()
+
+// arrow function
+let favnum = ()=>{
+var num9=798
+var num10=78
+return num9+num10
+
+}
+favnum()
+
+// Array
+let fruit=["Apple","Orange","Mango","banana","pineapple"]
+console.log(fruit[2])// Mango
+// Array can store multiple types
+let multipletype=["Badar",92,true,null,undefined]
+console.log(multipletype)
+
+//Push Pop
+let names=["Badar","Abbas","Ali","Zaid","Haroon"]
+names.pop()// delete Haroon from list
+console.log(names)
+names.push("Usman")// Add Usman at the end of list
+console.log(names)
+
+// Shift and Unshift
+let names1=["Badar","Abbas","Ali","Zaid","Haroon"]
+names1.shift()// delete Badar from list
+console.log(names1)
+
+names1.unshift("Usman")// Add Usman at the start of list
+console.log(names1)
+
+// Slice AND splice methode
+let names2=["Badar","Abbas","Ali","Zaid","Haroon"]
+let names3=["Usman","Zain","Zainab","Zainab","Zain"]
+// slice
+console.log(names2.splice(0,1,"BagaJuut","  Jutt saab"))// Abbas Ali
+// splice
+console.log(names2.splice(1,3))// Abbas Ali Zaid
+
+// Object
+var value = {
+    name:"Badar",
+    age: 20,
+    city: "Lahore",
+    country: "Pakistan"
+    }
+    console.log(value.name)
+    console.log(value.age)
+    console.log(value.city)
